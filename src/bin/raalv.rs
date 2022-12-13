87394e5e301e5d9a6eddbaee594ca3d9ccf2d5b0
LFalch <lucas@wasd.dk>
2022-12-13 12:29:14 +0100

    Enable relocatable linking

--- src/bin/raalv.rs
+++ src/bin/raalv.rs
@@ -1,4 +1,4 @@
-use std::env::args;
+use std::{env::args, collections::{HashSet, HashMap}};
 
 use telda2::{self, aalv::{obj::{ShebangAgnosticObject, GlobalSymbols, InternalSymbols, SymbolReferenceTable}, Segment}, source::Format};
 
@@ -6,11 +6,14 @@ fn main() {
     let arg = args().nth(1).unwrap();
 
     let obj = ShebangAgnosticObject::from_file(&arg).unwrap().into_object();
+    let mut defined_symbols = HashSet::new();
+    let mut undefined_symbols = HashMap::new();
     
     if let Some(v) = obj.global_symbols {
         println!("{}:", GlobalSymbols::NAME);
         for (l, a) in v.0 {
             println!("    {l} = 0x{a:02x}");
+            defined_symbols.insert(l);
         }
         println!();
     }
@@ -18,6 +21,7 @@ fn main() {
         println!("{}:", InternalSymbols::NAME);
         for (l, a) in v.0 {
             println!("    {l} = 0x{a:02x}");
+            defined_symbols.insert(l);
         }
         println!();
     }
@@ -30,6 +34,18 @@ fn main() {
                 Format::Big => print!("B"),
             }
             println!(" {l} @ 0x{a:02x}");
+            if !defined_symbols.contains(&l) {
+                *undefined_symbols.entry(l).or_insert(0u64) += 1;
+            }
+        }
+        println!();
+    }
+
+    if !undefined_symbols.is_empty() {
+        println!("undefined symbols:");
+        for (s, num) in undefined_symbols {
+            let pl = if num == 1 { "" } else { "s" };
+            println!("    {s} with {num} reference{pl}");
         }
         println!();
     }
--- src/bin/tl.rs
+++ src/bin/tl.rs
@@ -1,12 +1,12 @@
 use std::{path::PathBuf, collections::HashMap, process::ExitCode, fs::{self}, os::unix::prelude::PermissionsExt};
 
 use clap::Parser;
-use telda2::{source::Format, aalv::obj::{Object, GlobalSymbols, InternalSymbols}, mem::Lazy};
+use telda2::{source::Format, aalv::obj::{Object, GlobalSymbols, InternalSymbols, SymbolReferenceTable}, mem::Lazy};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Input binary files (with symbol and relocation table files implied)
+    /// Input telda object files
     input_files: Vec<PathBuf>,
 
     /// Sets the output path, otherwise a.out is used
@@ -14,24 +14,34 @@ struct Cli {
     out: Option<PathBuf>,
     
     /// Defines an entry-point and makes this output an executable binary
+    ///
+    /// Can be either a hexadecimal address prefixed by 0x or a symbol
     #[arg(short, long)]
     entry: Option<String>,
-    
-    /// Includes internal symbols and potentially other debug info to the object
-    #[arg(short = 'g', long)]
-    debug: bool,
-}
 
+    /// Removes internal symbols
+    #[arg(short = 'S', long)]
+    strip_internal: bool,
+
+    /// Includes references
+    ///
+    /// Without this, undefined symbols will cause an error
+    #[arg(short, long, conflicts_with = "strip_internal")]
+    relocatable: bool,
+}
 
 fn main() -> ExitCode {
     let Cli {
         input_files,
         out,
         entry,
-        debug
+        strip_internal,
+        relocatable,
     } = Cli::parse();
 
     let out = out.unwrap_or_else(|| PathBuf::from("a.to"));
+
+    let mut reloc_refs = Vec::new();
 
     let mut mem_out = Vec::new();
     let mut global_refs = Vec::new();
@@ -57,10 +67,13 @@ fn main() -> ExitCode {
 
         for (f, lbl, pos_in_file) in refs {
             let Some(&pos) = internal_labels.get(&*lbl) else {
-                global_refs.push((f, lbl, pos_in_file+offset));
+                global_refs.push((f, lbl.clone(), pos_in_file+offset));
                 continue;
             };
 
+            if relocatable {
+                reloc_refs.push((f, lbl, pos_in_file));
+            }
             rewrite(&mut mem_file, pos_in_file, f, pos+offset);
         }
 
@@ -74,7 +87,7 @@ fn main() -> ExitCode {
         }
 
         for (lbl, pos_in_file) in internal_labels {
-            local_symbols.push((lbl, pos_in_file));
+            local_symbols.push((lbl, pos_in_file+offset));
         }
 
         mem_out.extend(mem_file);
@@ -83,9 +96,14 @@ fn main() -> ExitCode {
     let mut undefined_references = false;
 
     for (f, lbl, pos) in global_refs {
+        if relocatable {
+            reloc_refs.push((f, lbl.clone(), pos));
+        }
         let Some(&new_value) = global_symbols.get(&*lbl) else {
-            eprintln!("undefined reference to {lbl} at 0x{pos:02x}");
-            undefined_references = true;
+            if !relocatable {
+                eprintln!("undefined reference to {lbl} at 0x{pos:02x}");
+                undefined_references = true;
+            }
             continue;
         };
 
@@ -118,8 +136,11 @@ fn main() -> ExitCode {
 
         obj.mem = Some(Lazy { mem: mem_out });
         obj.global_symbols = Some(GlobalSymbols(global_symbols.into_iter().collect()));
-        if debug {
+        if !strip_internal {
             obj.internal_symbols = Some(InternalSymbols(local_symbols));
+        }
+        if relocatable {
+            obj.symbol_reference_table = Some(SymbolReferenceTable(reloc_refs));
         }
         obj
     };
